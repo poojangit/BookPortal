af390e4ffe943dc3887177f0310ab43718bee095
poojangit <poojang147@gmail.com>
2024-06-05 14:56:57 +0530

    [POOJA] Modified : Modified add book function to check if the quantity is avalilable or not

--- BookPortal.js
+++ BookPortal.js
@@ -1,93 +1,93 @@
 const readlineSync = require('readline-sync')
 var BookDetails = [
-    {
-      bookId : 10 ,
-      bookName : "The Best of Us ",
-      price : 100,
-      bookStatus : "Available",
-      quantity : 1
-    },
-    {
-        bookId : 11,
-        bookName : "One Indian Girl",
-        price : 200,
-        bookStatus : "Unavalilable",
-        quantity : 1
-      },
-      {
-        bookId : 12,
-        bookName : "Half Girlfriend",
-        price : 500,
-        bookStatus : "Available",
-        quantity : 1
-      },
-      {
-        bookId : 13,
-        bookName : "The God of Small Things ",
-        price : 100,
-        bookStatus : "Available",
-        quantity : 1
-      },
-      {
-        bookId : 14,
-        bookName : "The White Tiger ",
-        price : 400,
-        bookStatus : "Unavalilable",
-        quantity : 1
-      },
-      {
-        bookId : 15,
-        bookName : "Revolution 2020",
-        price : 100,
-        bookStatus : "Unavalilable",
-        quantity : 1
-      },
-      {
-        bookId : 16,
-        bookName : "The 3 Mistakes of My Life",
-        price : 300,
-        bookStatus : "Available",
-        quantity : 1
-      },
-      {
-        bookId : 17,
-        bookName : "The Namesake",
-        price : 500,
-        bookStatus : "Available",
-        quantity : 5
-      },
-      {
-        bookId : 18,
-        bookName : "The Palace of Illusions",
-        price : 600,
-        bookStatus : "Unavalilable",
-        quantity : 1
-      }
+  {
+    bookId: 10,
+    bookName: "The Best of Us ",
+    price: 100,
+    bookStatus: "Available",
+    quantity: 1
+  },
+  {
+    bookId: 11,
+    bookName: "One Indian Girl",
+    price: 200,
+    bookStatus: "Unavalilable",
+    quantity: 1
+  },
+  {
+    bookId: 12,
+    bookName: "Half Girlfriend",
+    price: 500,
+    bookStatus: "Available",
+    quantity: 1
+  },
+  {
+    bookId: 13,
+    bookName: "The God of Small Things ",
+    price: 100,
+    bookStatus: "Available",
+    quantity: 1
+  },
+  {
+    bookId: 14,
+    bookName: "The White Tiger ",
+    price: 400,
+    bookStatus: "Unavalilable",
+    quantity: 1
+  },
+  {
+    bookId: 15,
+    bookName: "Revolution 2020",
+    price: 100,
+    bookStatus: "Unavalilable",
+    quantity: 1
+  },
+  {
+    bookId: 16,
+    bookName: "The 3 Mistakes of My Life",
+    price: 300,
+    bookStatus: "Available",
+    quantity: 1
+  },
+  {
+    bookId: 17,
+    bookName: "The Namesake",
+    price: 500,
+    bookStatus: "Available",
+    quantity: 5
+  },
+  {
+    bookId: 18,
+    bookName: "The Palace of Illusions",
+    price: 600,
+    bookStatus: "Unavalilable",
+    quantity: 1
+  }
 ]
 var cart = []
 function mainMenu() {
-    console.log("1 Show available Books");
-    console.log("2 Add Book")
-    console.log("3 Show cart");
-    console.log("4 Exit from the choice");
-    const choice = readlineSync.question("Please select an option: ")
+  console.log("1 Show available Books");
+  console.log("2 Add Book")
+  console.log("3 Show cart");
+  console.log("4 Exit from the choice");
+  const choice = readlineSync.question("Please select an option: ")
 
-switch (choice) {
-  case '1':
-    showBooks()
-    break;
-  case '2':
-    addBooks()
-    break
-  case '3':
-    showCart()
-    break
-  case '4' :
-    return
-  default:
-    console.log("You entered invalid choice");
-    break;
-}
+  switch (choice) {
+    case '1':
+      showBooks()
+      break;
+    case '2':
+      addBooks()
+      break
+    case '3':
+      showCart()
+      break
+    case '4':
+      return
+    default:
+      console.log("You entered invalid choice");
+      break;
+  }
 }
 
 mainMenu()
@@ -95,21 +95,20 @@ mainMenu()
 function showBooks() {
   console.log("Available Books to buy: ");
   BookDetails.forEach(book => {
-    if(book.quantity > 0)
-    book.bookStatus = "Available"
-  else {
-    book.bookStatus = "Unavailable"
-  }
-  if(book.bookStatus === "Available")
-      {
-        console.log()
-        console.log(`Book Id : ${book.bookId}`)
-        console.log(`Name : ${book.bookName} `)
-        console.log(`Price : ${book.price} ` ) 
-        console.log(`Quantity: ${book.quantity}`);
-        console.log("-----------------------------------")
+    if (book.quantity > 0)
+      book.bookStatus = "Available"
+    else {
+      book.bookStatus = "Unavailable"
+    }
+    if (book.bookStatus === "Available") {
+      console.log()
+      console.log(`Book Id : ${book.bookId}`)
+      console.log(`Name : ${book.bookName} `)
+      console.log(`Price : ${book.price} `)
+      console.log(`Quantity: ${book.quantity}`);
+      console.log("-----------------------------------")
 
-      }
+    }
   })
 }
 mainMenu()
@@ -119,38 +118,48 @@ function addBooks() {
   const book = BookDetails.find(book => book.bookId === bookId);
 
   if (book && book.bookStatus === "Available" && book.quantity > 0) {
+    while (true) {
       const quantity = parseInt(readlineSync.question(`Enter the quantity to add to cart (Available: ${book.quantity}): `));
-      
-      if (quantity > 0 && quantity <= book.quantity) {
-          cart.push({
-              bookId: book.bookId,
-              bookName: book.bookName,
-              price: book.price,
-              quantity: quantity
-          });
-          book.quantity -= quantity;
 
-          console.log(`Book "${book.bookName}" added to the cart. Quantity: ${quantity}`);
+      if (quantity > 0 && quantity <= book.quantity) {
+        const cartItem = cart.find(item => item.bookId === bookId)
+
+        if (cartItem) {
+          cartItem.quantity += quantity
+        }
+        else {
+          cart.push({
+            bookId: book.bookId,
+            bookName: book.bookName,
+            price: book.price,
+            quantity: quantity
+          });
+        }
+        book.quantity -= quantity;
+
+        console.log(`Book "${book.bookName}" added to the cart. Quantity: ${quantity}`);
+        break
       } else {
-          console.log("Invalid quantity. Please enter a quantity within the available range.");
+        console.log("Invalid quantity. Please enter a quantity within the available range.");
       }
+    }
   } else {
-      console.log("Book is not available or invalid book ID.");
+    console.log("Book is not available or invalid book ID.");
   }
 }
 mainMenu()
 function showCart() {
   console.log("Cart Items:");
   if (cart.length === 0) {
-      console.log("Your cart is empty.");
+    console.log("Your cart is empty.");
   } else {
     let totalCartValue = 0;
-      cart.forEach(item => {
-        const totalPrice = item.price * item.quantity;
-        totalCartValue += totalPrice;
-          console.log(`ID: ${item.bookId}, Name: ${item.bookName}, Price: ${item.price}, Quantity: ${item.quantity}, Total Price: ${totalPrice}`);
-      });
-      console.log(`Total Cart Value: ${totalCartValue}`);
+    cart.forEach(item => {
+      const totalPrice = item.price * item.quantity;
+      totalCartValue += totalPrice;
+      console.log(`ID: ${item.bookId}, Name: ${item.bookName}, Price: ${item.price}, Quantity: ${item.quantity}, Total Price: ${totalPrice}`);
+    });
+    console.log(`Total Cart Value: ${totalCartValue}`);
   }
 }
 mainMenu()
